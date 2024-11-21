af586b73856412ff618669899598e3e8a1927808
PabloChorda <pablochorda365@gmail.com>
2024-11-21 23:44:58 +0100

    este es

---
+++ 05_operaciones.py
@@ -0,0 +1,18 @@
+''' EJERCICIO:
+Crea un programa que explore diferentes operaciones que se pueden 
+realizar con listas en tu lenguaje de programación. Algunas operaciones 
+posibles son:
+
+Indexación, slicing, adición, eliminación, búsqueda, ordenación, inversión, conteo de elementos, comprobación de unicidad, concatenación y copia.
+Trabaja con listas anidadas e intenta modificar elementos dentro de estas.
+DIFICULTAD EXTRA (opcional):
+Implementa un programa que:
+
+Genere dos listas de números aleatorios de longitud variable.
+Compare ambas listas para determinar:
+Los elementos comunes.
+Los elementos únicos en cada lista.
+Si una es una subsecuencia de la otra.
+Calcule la suma acumulativa y el producto de los elementos de cada lista.
+
+'''
