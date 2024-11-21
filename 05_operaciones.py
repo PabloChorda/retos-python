8d2387f740785a0ba04549df3b252b0d83ee454b
PabloChorda <pablochorda365@gmail.com>
2024-11-21 23:49:05 +0100

    Ejercicio 05

--- 05_operaciones.py
+++ 05_operaciones.py
@@ -5,15 +5,6 @@ posibles son:
 
 Indexación, slicing, adición, eliminación, búsqueda, ordenación, inversión, conteo de elementos, comprobación de unicidad, concatenación y copia.
 Trabaja con listas anidadas e intenta modificar elementos dentro de estas.
-DIFICULTAD EXTRA (opcional):
-Implementa un programa que:
-
-Genere dos listas de números aleatorios de longitud variable.
-Compare ambas listas para determinar:
-Los elementos comunes.
-Los elementos únicos en cada lista.
-Si una es una subsecuencia de la otra.
-Calcule la suma acumulativa y el producto de los elementos de cada lista.
 
 '''
 
