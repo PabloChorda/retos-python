ac0dcb26398eaf9c5d70921d74f8cd211abf5c47
PabloChorda <pablochorda365@gmail.com>
2024-11-21 23:53:07 +0100

    N UEVO

---
+++ 06_analiza_frases.py
@@ -0,0 +1,10 @@
+'''
+EJERCICIO:
+Crea un programa que lea una frase ingresada por el usuario y determine:
+
+La palabra más larga de la frase.
+La cantidad de palabras que contienen más de 5 letras.
+Si todas las palabras de la frase son únicas (es decir, 
+si no hay palabras repetidas).
+'''
+
