4b8504f367241834f82d63221616c4ea0fea055a
PabloChorda <pablochorda365@gmail.com>
2024-11-22 23:56:03 +0100

    sistema votacion

---
+++ 07_sistema_votacion.py
@@ -0,0 +1,16 @@
+
+'''
+Aquí tienes un ejercicio más desafiante, que requiere lógica y manejo de estructuras de datos:
+
+EJERCICIO:
+Crea un programa que simule un sistema de votación. El programa debe:
+
+Permitir que los usuarios ingresen el nombre de los candidatos (mínimo 2 candidatos).
+Realizar una votación simulada con n votos (donde n es ingresado por el usuario). 
+Cada voto se asignará aleatoriamente a uno de los candidatos.
+Mostrar al final:
+El número total de votos que recibió cada candidato.
+El candidato ganador.
+Si hubo empate, muestra los candidatos empatados.
+
+'''
