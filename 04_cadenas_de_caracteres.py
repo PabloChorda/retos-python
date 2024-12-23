50b6ac0611c76f06ae5d1dec3a05faaf8cd84cab
PabloChorda <pablochorda365@gmail.com>
2024-12-23 23:32:36 +0100

    04_cadenas_de_caracteres.py

--- 04_cadenas_de_caracteres.py
+++ 04_cadenas_de_caracteres.py
@@ -4,7 +4,7 @@ Muestra ejemplos de todas las operaciones que puedes realizar con cadenas de car
 en tu lenguaje. Algunas de esas operaciones podrían ser (busca todas las que puedas):
 - Acceso a caracteres específicos, subcadenas, longitud, concatenación, repetición,
   recorrido, conversión a mayúsculas y minúsculas, reemplazo, división, unión,
-  interpolación, verificación...
+  interpolación, verificación, etc.
 DIFICULTAD EXTRA (opcional):
 Crea un programa que analice dos palabras diferentes y realice comprobaciones
 para descubrir si son:
